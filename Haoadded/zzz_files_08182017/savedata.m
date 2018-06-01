fac489aba1832bdc10cbaff5cc1f5291be43d964
chenhao1UMBC <chenhao1@umbc.edu>
2018-06-01 03:42:43 -0700

    small change for naming issue in the savedata

--- Haoadded/zzz_files_08182017/savedata.m
+++ Haoadded/zzz_files_08182017/savedata.m
@@ -3,16 +3,21 @@ db = Hprepare_database(src,feature_fun,opt,SNR);
 db2 = Hprepare_database2(src,feature_fun,opt,SNR);
 
 if opt.save == 1
-    nm1 = ['db',num2str(nsamples),'_',num2str(C),'classq',num2str(q1), '&', num2str(q2),'n',...
-        num2str(n), '_positive_M',num2str(options.M),'_snr', num2str(SNR)];
-    nm2 = ['db',num2str(nsamples),'_',num2str(C),'classq',num2str(q1), '&', num2str(q2),'n',...
-        num2str(n), '_negative_M',num2str(options.M),'_snr', num2str(SNR)];
+
     if opt.norm == 0
         if opt.mix
             nm1=['mix',num2str(opt.cn),nm1,'power_',num2str(opt.cndb), '.mat'];
             nm2=['mix',num2str(opt.cn),nm2,'power_',num2str(opt.cndb), '.mat'];
         end
+        nm1 = ['db',num2str(nsamples),'_',num2str(C),'classq',num2str(q1), '&', num2str(q2),'n',...
+            num2str(n), '_positive_M',num2str(options.M),'_snr', num2str(SNR)];
+        nm2 = ['db',num2str(nsamples),'_',num2str(C),'classq',num2str(q1), '&', num2str(q2),'n',...
+            num2str(n), '_negative_M',num2str(options.M),'_snr', num2str(SNR)];
     else
+        nm1 = ['norm_db',num2str(nsamples),'_',num2str(C),'classq',num2str(q1), '&', num2str(q2),'n',...
+            num2str(n), '_positive_M',num2str(options.M),'_snr', num2str(SNR)];
+        nm2 = ['norm_db',num2str(nsamples),'_',num2str(C),'classq',num2str(q1), '&', num2str(q2),'n',...
+            num2str(n), '_negative_M',num2str(options.M),'_snr', num2str(SNR)];
         if opt.mix
             nm1=['norm_mix',num2str(opt.cn),nm1,'power_',num2str(opt.cndb), '.mat'];
             nm2=['norm_mix',num2str(opt.cn),nm2,'power_',num2str(opt.cndb), '.mat'];
